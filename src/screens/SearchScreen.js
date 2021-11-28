9c760459a1623b441b1c73021b8b59dbc9b71606
melange <nikolaivan@mail.ru>
2021-11-28 11:54:29 +0300

    search bar added

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <SearchBar/>
+           <Text>Search Screen</Text>
+        </View>  
+    );
+};
+
+export default SearchScreen;
